c8e9bc3e0a8fa495f63d83d9c7474f63d2994d77
MaximSakharov <145155919+MaximSakharov@users.noreply.github.com>
2023-12-17 23:46:32 +0300

    Add files via upload

---
+++ Module 2/exercise 2/Train.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/**
+* @brief Выводит данные из файла о поездах, пребывающих в пути более суток
+* @param filename_ : Название файла
+*/
+void printValidTrains(const char* filename_);
