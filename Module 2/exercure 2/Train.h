64ca508842eafce513d36cf0633a3fcffffcf8cf
MaximSakharov <145155919+MaximSakharov@users.noreply.github.com>
2023-12-17 23:48:20 +0300

    Delete Module 2/exercure 2 directory

--- Module 2/exercure 2/Train.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/**
-* @brief Выводит данные из файла о поездах, пребывающих в пути более суток
-* @param filename_ : Название файла
-*/
-void printValidTrains(const char* filename_);
